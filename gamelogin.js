1133c8e25a9507313eb889523294b1999ee82d5c
Arkesh-Unity <66406891+Arkesh-Unity@users.noreply.github.com>
2020-12-18 15:17:10 +0530

    Update gamelogin.js

--- gamelogin.js
+++ gamelogin.js
@@ -1,5 +1,6 @@
 
 
+import * as storageJs from './storage.js';
 function myFunction() {
     //window.location.href="home.html" 
     
@@ -18,6 +19,9 @@ function myFunction() {
        console.log("krishna login result str.gameUserData.playerId "+str.gameUserData.playerId);
        console.log("krishna login result str.gameUserData.playerLevel "+str.gameUserData.playerLevel);
        console.log("krishna login result str.gameUserData.ts "+str.gameUserData.ts);
+// {"code":0,"data":"Login SUCCESS", "gameUserData":{"displayName":"xx","gameAuthSign":"xx","isAuth":1,"playerId":"xx","playerLevel":x,"ts":"xxx"}}
+        storageJs.saveLoginDetails(str.code,str.data,str.gameUserData.displayName,str.gameUserData.gameAuthSign,
+        str.gameUserData.isAuth,str.gameUserData.playerId,str.gameUserData.playerLevel,str.gameUserData.ts);
 
        document.location.href="https://htmlpreview.github.io/?https://github.com/Arkesh-Unity/QuickGameSampleTesting/blob/main/home.html"
       
