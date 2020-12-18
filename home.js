499f90fd445fd0415611ecf7d9b198df9c292691
Arkesh-Unity <66406891+Arkesh-Unity@users.noreply.github.com>
2020-12-18 15:19:44 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,20 @@
+import * as storageJs from './storage.js';
+
+function myPlay() {
+    window.location.href="bubble-shooter-changes.html" }
+
+     // function myPlay() {
+     // $("#myModal").modal({backdrop: true}); }
+
+     //function myLogout() {
+     // window.location.href=".html" }
+     function myLeaderboard() {
+     window.location.href="leaderboard.html"
+    
+     
+    }
+     //function mytore() {
+     //window.location.href="save.html" }
+       
+  function myStore() {
+     window.location.href="iap.html" }
